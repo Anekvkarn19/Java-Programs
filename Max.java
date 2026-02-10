08e811d5769b096f0bdaf873b028913884895770
Anekvarna <anek.varna030@gmail.com>
2026-02-10 14:17:14 +0530

    Added Max.java

---
+++ Max.java
@@ -0,0 +1,19 @@
+public class Max 
+{
+    public static void main(String[] args) 
+    {
+        int num1 = 15;
+        int num2 = 25;
+        int max;
+        
+        if(num1>num2)
+        {
+             max = num1;
+        }
+        else
+        {
+             max = num2;
+        }
+        System.out.println("The maximum of " + num1 + " and " + num2 + " is: " + max);
+    }
+}
