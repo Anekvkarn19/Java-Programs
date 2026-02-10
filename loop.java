25ae0541f06e9d0f13971579f7d4ab6f79c7d7a2
Anekvarna <anek.varna030@gmail.com>
2026-02-10 14:17:14 +0530

    Added loop.java

---
+++ loop.java
@@ -0,0 +1,19 @@
+public class loop
+ {
+    int a;
+    public static void main(String[] args) 
+    {
+        // for (int i = 1; i <=5; i++) 
+        // {
+        //     System.out.println("Hello");
+        // }
+    //   loop l=new loop();
+    //     System.out.println(l.a);
+    //boolean t=true;
+    for(int i=0;true;i++)
+        {
+       System.out.println("Hii");
+        }    
+    }
+    
+}
