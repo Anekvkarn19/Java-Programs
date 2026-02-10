af4318d595f4d43e74840b4a7cfaf74e759e0ec3
Anekvarna <anek.varna030@gmail.com>
2026-02-10 14:17:13 +0530

    Added EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,15 @@
+public class EvenOdd {
+    public static void main(String args[])
+    {
+        int n=38;
+        if(n%2==0)
+        {
+            System.out.println("The number " + n + " is an Even Number");
+        }
+        else
+        {
+            System.out.println("The number " + n + " is an Odd Number");
+        }
+
+     }
+}
